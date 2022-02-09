8052bab3f2812e42cf6da2b832fbb6c98d641be3
Sandipan Dey <hey@sandipan.dev>
2022-02-09 22:25:07 +0530

    ci: fix script

--- ci/tasks/prep-release.sh
+++ ci/tasks/prep-release.sh
@@ -62,5 +62,6 @@ jq --arg v "$(cat ../version/version)" '.version = $v' package.json > ../tmp && 
 git add package.json
 git status
 
-if [[ -z $(git status -s) ]]
+if [[ -z $(git status -s) ]]; then
   git commit -m "ci(release): release version $(cat ../version/version)"
+fi
