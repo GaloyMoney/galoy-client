3072cf720667ad3dbf773fcfe448f85cb4487476
Samer Buna <75209+samerbuna@users.noreply.github.com>
2022-02-17 15:53:45 -0800

    feat: add more messages and helper functions (#15)
    Co-authored-by: Samer Buna <samerbuna@users.noreply.github.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -50,3 +50,18 @@ export const formatUsd = (value: number) => {
 }
 
 export const satsToBTC = (satsAmount: number): number => satsAmount / 10 ** 8
+
+export const initialsDisplay = (text: string): string => {
+  return text.substring(0, 1).toUpperCase()
+}
+
+export const truncatedDisplay = (
+  text: string,
+  { max = 15 }: { max?: number } = {},
+): string => {
+  if (text.length > max) {
+    return text.substring(0, max) + "..."
+  }
+
+  return text
+}
