b4f361e44d3ba14dd3e65fc80a21233901879a35
Samer Buna <samerbuna@users.noreply.github.com>
2022-03-31 18:24:41 -0700

    fix: messages import

--- src/translate/index.ts
+++ src/translate/index.ts
@@ -2,8 +2,8 @@ import i18n from "i18n-js"
 
 import ES from "./es"
 
-import * as es from "./es-m"
-import * as en from "./en-m"
+import es from "./es-m"
+import en from "./en-m"
 
 i18n.fallbacks = true
 i18n.translations = { es: { ...es, ...ES }, en }
@@ -21,7 +21,7 @@ export const translate: GaloyTranslate = (scope, options) => {
 export type GaloyTranslateUnknown = (
   scope: string,
   options?: i18n.TranslateOptions | undefined,
-) => string
+) => string | Record<string, string | Record<string, any>>
 
 export const translateUnknown: GaloyTranslateUnknown = (scope, options) => {
   const translation = i18n.t(scope, { defaultValue: scope, ...options })
