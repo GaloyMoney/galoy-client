306a3053cafde51925e765c1deddbd439a6091e4
Samer Buna <samerbuna@users.noreply.github.com>
2022-08-17 15:13:13 -0700

    chore: remove i18n from client

--- src/translate/index.ts
+++ src/translate/index.ts
@@ -1,45 +1,9 @@
-import i18n from "i18n-js"
-
-import esMain from "./es-main"
-
-import en from "./en"
-import es from "./es"
-import ptBR from "./pt-br"
-import frCA from "./fr-ca"
-
-i18n.fallbacks = true
-i18n.translations = { es: { ...es, ...esMain }, pt: ptBR, fr: frCA, en }
-
-export type GaloyTranslate = (
-  scope: keyof typeof esMain,
-  options?: i18n.TranslateOptions | undefined,
-) => string
-
-export const translate: GaloyTranslate = (scope, options) => {
-  const translation = i18n.t(scope, { defaultValue: scope, ...options })
-  return translation
-}
-
-export type GaloyTranslateUnknown = (
-  scope: string,
-  options?: i18n.TranslateOptions | undefined,
-) => any
-
-export const translateUnknown: GaloyTranslateUnknown = (scope, options) => {
-  const translation = i18n.t(scope, { defaultValue: scope, ...options })
-  return translation
-}
-
-export const setLocale = (language: string | undefined): void => {
-  if (language && language !== "DEFAULT" && i18n.locale !== language) {
-    i18n.locale = language
-  }
-}
-
-export const getLocale = (): string => {
-  return i18n.locale
-}
-
-export { toNumber as toLocaleNumber } from "i18n-js"
-
+// Web Wallet Messages (Default keys for English)
 export * from "./es-main"
+export { default as LANG_ES_MAIN } from "./es-main"
+
+// Mobile Wallet Messages (No default keys)
+export { default as LANG_EN_US } from "./en"
+export { default as LANG_ES_SV } from "./es"
+export { default as LANG_PT_BR } from "./pt-br"
+export { default as LANG_FR_CA } from "./fr-ca"
