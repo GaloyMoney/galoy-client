1e40d74e642302c49f96c2728c8c8478f7d54857
nicolasburtey <nicolasburtey@users.noreply.github.com>
2022-12-31 17:20:30 +0000

    chore: moving rest of the files from .ts to .gql (#178)

--- src/graphql/queries/user-default-wallet-id.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client"
-
-const userDefaultWalletId = gql`
-  query userDefaultWalletId($username: Username!) {
-    userDefaultWalletId(username: $username)
-  }
-`
-
-export default userDefaultWalletId
