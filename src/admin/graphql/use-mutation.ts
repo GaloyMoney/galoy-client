1be6fab3ee0d73353dc9b4933ad7604406b836be
Nicolas Burtey <nicolas.burtey@gmail.com>
2023-01-01 14:55:41 +0000

    chore: missing conversion to gql of admin queries

--- src/admin/graphql/use-mutation.ts
+++ src/admin/graphql/use-mutation.ts
@@ -1,5 +1,7 @@
-import accountUpdateLevel from "./mutations/account-update-level"
-import accountUpdateStatus from "./mutations/account-update-status"
+import { loadFilesSync } from "@graphql-tools/load-files"
+
+const accountUpdateStatus = loadFilesSync("./mutations/account-update-status.gql")[0]
+const accountUpdateLevel = loadFilesSync("./mutations/account-update-level.gql")[0]
 
 export const MUTATIONS = {
   accountUpdateStatus,
