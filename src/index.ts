b9e68a5cb5ebdff782daa708b435a90612ca48cb
Nicolas Burtey <nicolas.burtey@gmail.com>
2023-01-11 13:27:06 +0000

    chore: more code deletino

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-export * from "./client/index"
-export * from "./graphql/index"
 export * from "./translate/index"
 export * from "./utils/index"
 export * from "./parsing/index"
 export * from "./lnurl/index"
 export * as parsingv2 from "./parsing-v2/index"
-
-export * as admin from "./admin/graphql/index"
